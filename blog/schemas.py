1a95c5b2d1ca22676a7963f4227bcd226c8c315e
Bhargav Anand <bhargav2003anand@gmail.com>
2025-05-22 22:24:04 +0530

    FASTAPI

---
+++ blog/schemas.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel
+from typing import List
+
+class BlogBase(BaseModel):
+    title: str
+    body: str
+
+class Blog(BlogBase):
+    class Config():
+        orm_mode = True
+
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    blog : List[Blog]
+
+    class Config():
+        orm_mode = True
+
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    owner: ShowUser
+    
+    class Config():
+        orm_mode = True
+
+class Login(BaseModel):
+    username: str
+    password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    email: str | None = None
